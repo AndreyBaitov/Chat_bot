8d190b4974611d11a3dacf5c210dbdc8ff3f2616
Доктор Зло <dgagak@mail.ru>
2022-01-08 11:22:01 +0300

    Убрал падение скрипта при вводе команд во время настройки игры

--- sea_battle.py
+++ sea_battle.py
@@ -421,35 +421,38 @@ class SeaBattle:
            предположим мы будем хранить текущее состояние в отдельной переменной'''
         message = message.lower()
 
-        # Обработка команд боту todo обработка команд может быть только в игре, но не при старте
-        if any([_ in message for _ in ['выход','выйти','конец','надоел','сдаюсь']]):
-            answer = self.result_game()
-            self.stage = 'end the game'
-            return answer
-        elif any([_ in message for _ in ['сохрани']]):
-            bots_ships, bots_score = self.count_result_game(self.remaining_bots_ships, self.bot_board)
-            user_ships, users_score = self.count_result_game(self.remaining_users_ships, self.lazy_user_board)
-            return f'У меня {bots_ships}\nУ тебя {user_ships}\nСчёт: {bots_score} - {users_score}.\nСохраняю игру!'
-        elif any([_ in message for _ in ['покажи', 'показать']]):  # проверка показ полей
-            self.show_users_boards()
-            return 'Океюшки'
-        elif any([_ in message for _ in ['показывай поле каждый ход', 'каждый ход']]):
-            self.show_every_turn = True
-            return 'Океюшки'
-        elif any([_ in message for _ in ['помощь', 'помошь', 'помоги', 'хелп']]):
-            return 'Чтобы выйти, набери "выход".\nЧтобы получить список сохравнишхся в живых кораблей, набери "корабли"\nЧтобы увидеть свои поля, набери "покажи"'
-        elif any([_ in message for _ in ['корабли', 'какие', 'сколько', 'кораблей', 'какой', 'счёт']]):
-            bots_ships, bots_score = self.count_result_game(self.remaining_bots_ships, self.bot_board)
-            user_ships, users_score = self.count_result_game(self.remaining_users_ships, self.lazy_user_board)
-            return f'У меня {bots_ships}\nУ тебя {user_ships}\nСчёт: {bots_score} - {users_score}'
+        # Обработка команд боту
+        try:  #обработка команд может быть только в игре, но не при старте, при старте будет вываливаться ошибка
+            if any([_ in message for _ in ['выход','выйти','конец','надоел','сдаюсь']]):
+                answer = self.result_game()
+                self.stage = 'end the game'
+                return answer
+            elif any([_ in message for _ in ['сохрани']]):
+                bots_ships, bots_score = self.count_result_game(self.remaining_bots_ships, self.bot_board)
+                user_ships, users_score = self.count_result_game(self.remaining_users_ships, self.lazy_user_board)
+                return f'У меня {bots_ships}\nУ тебя {user_ships}\nСчёт: {bots_score} - {users_score}.\nСохраняю игру!'
+            elif any([_ in message for _ in ['покажи', 'показать']]):  # проверка показ полей
+                self.show_users_boards()
+                return 'Океюшки'
+            elif any([_ in message for _ in ['показывай поле каждый ход', 'каждый ход']]):
+                self.show_every_turn = True
+                return 'Океюшки'
+            elif any([_ in message for _ in ['помощь', 'помошь', 'помоги', 'хелп']]):
+                return 'Чтобы выйти, набери "выход".\nЧтобы получить список сохравнишхся в живых кораблей, набери "корабли"\nЧтобы увидеть свои поля, набери "покажи"'
+            elif any([_ in message for _ in ['корабли', 'какие', 'сколько', 'кораблей', 'какой', 'счёт']]):
+                bots_ships, bots_score = self.count_result_game(self.remaining_bots_ships, self.bot_board)
+                user_ships, users_score = self.count_result_game(self.remaining_users_ships, self.lazy_user_board)
+                return f'У меня {bots_ships}\nУ тебя {user_ships}\nСчёт: {bots_score} - {users_score}'
+        except Exception:
+            return 'Не понял'
 
-        answer = self.stage(message)
+        answer = self.stage(message)    # основная часть работы, в переменной хранится соответствующая функция
 
-        if self.show_every_turn:  # показывать каждый ход
+        if self.show_every_turn:        # показ поля в терминале
             self.show_users_boards()
 
         return answer
-    #todo при победе дублируется сообщение о победе
+    #todo при победе дублируется сообщение о победе0
     def check_lazy_users_board(self, answer: str) -> str:
         '''Проверяет за игрока, попал ли бот, и дополняет ответ, пока бот не промахнётся'''
 
