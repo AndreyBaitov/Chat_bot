c69ecd90ac2275b917cdee0b105c1152991b4ac1
Доктор Зло <dgagak@mail.ru>
2022-01-09 19:07:52 +0300

    Добавил отправку письма админу от бота при получении заявки

--- bots_handlers.py
+++ bots_handlers.py
@@ -1,5 +1,7 @@
 import re
 import time
+import settings
+from email_sender import email_sender
 
 name_pattern = re.compile(r'[А-Я,а-я]{2,20}')
 email_pattern = re.compile(r'^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$')
@@ -24,12 +26,15 @@ def handler_email(email: str, context: dict):
 
 def handler_agree_regulations(message: str, context: dict):
     '''Получает на вход да или нет, если да, логирует заявку -> True, если нет, возвращает False'''
+
     message = message.lower()
     if any([_ in message for _ in ['да','lf','ага','fuf','yes']]):
         time_now = time.gmtime(time.time() - time.timezone)
         date_time = time.strftime('%d.%m.%Y %H:%M:%S', time_now)
+        record = f'Камрад {context["name"]} с электронной почтой {context["email"]} подал заявку на вступление в клуб {date_time}.\n'
         with open('log/claims_for_club.txt','a', encoding='utf-8') as file:
-            file.write(f'Камрад {context["name"]} с электронной почтой {context["email"]} подал заявку на вступление в клуб {date_time}.\n')
+            file.write(record)
+        email_sender(message=record, subject='Заявка на вступление в клуб')
         return True
     return False
 
